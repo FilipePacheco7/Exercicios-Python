940874e665f68a2cc29463ba6041d39dbdb0f312
FilipePacheco7 <filipepacheco795@gmail.com>
2025-06-01 21:36:57 -0300

    Exercícios do primeiro dia de treinamento - 01/06/2025

---
+++ ex003.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite o primeiro valor:\n'))
+n2 = int(input('Digite o segundo valor:\n'))
+
+soma = n1 + n2
+
+print('{} + {} = {}'.format(n1, n2, soma))
